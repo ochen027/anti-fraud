d2081a75d2042e43770aa84722e01b18f2ced818
wong.huang <wong.huang@pwc.com>
2017-06-28 14:25:03 +0800

    change structure

--- src/main/java/com/pwc/aml/controller/IndexController.java
+++ src/main/java/com/pwc/aml/controller/IndexController.java
@@ -1,8 +1,12 @@
 package com.pwc.aml.controller;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 import java.util.Date;
 import java.util.Map;
@@ -10,13 +14,12 @@ import java.util.Map;
 
 
 @Controller
-@RequestMapping("/index")
 public class IndexController {
 
 
-    @GetMapping("/login")
-    public String welcome(Map<String, Object> model) {
-
+    @RequestMapping("/greeting")
+    public String greeting(Model model) {
+        //model.addAttribute("name", name);
         return "index";
     }
 
