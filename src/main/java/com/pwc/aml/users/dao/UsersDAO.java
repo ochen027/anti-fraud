ca5b20dfe3b5b958054b61449fafd40216deaca6
ochen027 <orvin.chen@pwc.com>
2017-07-05 13:34:41 +0800

    Login - Get Group Role Information - JSON

--- src/main/java/com/pwc/aml/users/dao/UsersDAO.java
+++ src/main/java/com/pwc/aml/users/dao/UsersDAO.java
@@ -32,20 +32,18 @@ public class UsersDAO implements  IUsersDAO{
     }
 
     @Override
-    public List<UserGroupRoleBean> fetchUserGroups(int userId) {
-        String sql = "SELECT U.USER_ID, U.USER_NAME, G.GROUP_NAME, G.GROUP_ID " +
+    public List fetchUserGroups(int userId) {
+        String sql = "SELECT DISTINCT G.GROUP_ID, G.GROUP_NAME " +
                 "FROM USERS AS U, GROUPS AS G, USERGROUP AS UG" +
                 " WHERE U.USER_ID = UG.USER_ID AND G.GROUP_ID = UG.GROUP_ID AND U.STATUS = 1 AND G.STATUS = 1 AND U.USER_ID = ?";
-
-        return  entityManager.createNativeQuery(sql).setParameter(1, userId).getResultList();
+        return entityManager.createNativeQuery(sql).setParameter(1, userId).getResultList();
     }
 
     @Override
-    public List<UserGroupRoleBean> fetchGroupRoles(int groupId) {
-        String sql = "SELECT G.GROUP_NAME, G.GROUP_ID, R.ROLE_ID, R.ROLE_NAME " +
+    public List fetchGroupRoles(int groupId) {
+        String sql = "SELECT DISTINCT R.ROLE_ID, R.ROLE_NAME " +
                 "FROM ROLES AS R, GROUPS AS G, GROUPROLE AS GR" +
                 " WHERE R.ROLE_ID = GR.ROLE_ID AND G.GROUP_ID = GR.GROUP_ID AND R.STATUS = 1 AND G.STATUS = 1 AND G.GROUP_ID = ?";
-
         return entityManager.createNativeQuery(sql).setParameter(1, groupId).getResultList();
     }
 
