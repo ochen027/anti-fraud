0040ea2a0ee01e2fd85006f55211f638383c59ed
wong.huang <wong.huang@pwc.com>
2017-07-06 10:28:35 +0800

    change name

--- src/main/java/com/pwc/aml/transation/controller/TransactionRestController.java
+++ src/main/java/com/pwc/aml/transation/controller/TransactionRestController.java
@@ -18,7 +18,7 @@ import java.util.Map;
 
 @Controller
 @RequestMapping("transaction")
-public class TransactionController {
+public class TransactionRestController {
     @Autowired
     private ITransactionService transactionServiceImp;
 
