70e442af828254580cff08ee0913cb7b619f2ea8
wong.huang <wong.huang@pwc.com>
2017-07-07 17:30:53 +0800

    add workflow structure

---
+++ src/main/java/com/pwc/aml/workflow/controller/WorkflowController.java
@@ -0,0 +1,26 @@
+package com.pwc.aml.workflow.controller;
+
+
+import com.pwc.aml.workflow.entity.Workflow;
+import com.pwc.aml.workflow.service.IWorkflowService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+
+@Controller
+@RequestMapping("workflow")
+public class WorkflowController {
+    @Autowired
+    private IWorkflowService usersService;
+
+    @PostMapping("get")
+    public ResponseEntity<Void> get(@RequestBody Workflow u){
+
+    	return new ResponseEntity<Void>(HttpStatus.OK);
+    }
+    
+    
+}
