235e1f7fe7a6c4883c1c3360763b35679dbe4b10
wong.huang <wong.huang@pwc.com>
2017-07-10 16:36:15 +0800

    get roles

--- src/main/resources/static/js/workflow/controllers/workflow.js
+++ src/main/resources/static/js/workflow/controllers/workflow.js
@@ -1,85 +1,16 @@
 window.targetProperty = new Object();
 window.project = new Object();
 
-//for data transfer
-var self = {
-    flowPoint: function (obj) {
-        var j = {
-            uid: obj.id,
-            type: obj.type, //obj.userData.type or obj.type
-            name: obj.userData.name,
-            print: obj.userData.name + "(" + obj.userData.customerID + ")",
-            flowid: obj.userData.customerID,
-            rGuid: obj.userData.rGuid,
-            api: '',
-            events: [],
-            description: '',
-            attributes: {
-                x: obj.x,
-                y: obj.y,
-                height: obj.height,
-                width: obj.width,
-                color: obj.color,
-                shape: obj.userData.type
-            }
-        }
-        return j
-    },
-    flowEvent: function (obj) {
-        var j = {
-            uid: obj.id, //obj.labels.type
-            type: obj.type,
-            name: obj.userData.name,
-            print: obj.labels.length > 0 ? obj.labels[0].labelData.text : "",
-            flowid: obj.userData.customerID,
-            api: '',
-            endpoint: obj.target.node,
-            description: '',
-        }
-        return j;
 
-    },
-    draw2dToModel: function (draw2dObj) {
-        var flowEvents = [];
-        var tempEvents = {};
-        var tempPoints = {};
-        var staticMapping = {
-            "WorkflowShape.WorkflowStart": 1,
-            "WorkflowShape.WorkflowEnd": 1,
-            "WorkflowShape.WorkflowProcess": 1,
-            "WorkflowShape.Connection": 2,
-        }
-        for (var i in draw2dObj) {
-            var o = draw2dObj[i];
-            if (staticMapping[o.type] === 1) {
-                tempPoints[o.id] = self.flowPoint(o);
-            } else {
-                tempEvents[o.id] = o;
-            }
-        }
-
-
-        for (var key in tempEvents) {
-            var o = tempEvents[key];
-            flowEvents.push(self.flowEvent(o));
-            tempPoints[o.source.node].events.push(o.id);
-        }
-
-        var flowPoints = Object.keys(tempPoints).map(function (k) {
-            return tempPoints[k]
-        });
-
-        return {"flowEvents": flowEvents, "flowPoints": flowPoints}
-
-    }
-}
 
 
 app.controller('EditWorkflowCtrl', function ($scope, $http, $location,$cookies,$cookieStore, $state, $timeout,$loginService) {
 
+
+
+
+
     $timeout(function(){
-
-
         var initClick = function () {
             saveProjectClick();
             btnCancelClick();
@@ -98,7 +29,6 @@ app.controller('EditWorkflowCtrl', function ($scope, $http, $location,$cookies,$
          */
         var initial = function () {
             Workflow.init("canvas", targetProperty);
-
             window.flowId = $("#flowUID").val();
             initialEdit(flowId);
             initialRole();
