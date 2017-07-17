822a5460b852adbc62f62c8513fd6b1a8a3c9ee6
wong.huang <wong.huang@pwc.com>
2017-07-17 15:21:52 +0800

    change import sql

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -53,13 +53,21 @@ insert into menus values(4, 'sysadmin', sysdate, sysdate, 'sysadmin', 1, NULL , 
 insert into menus values(5, 'sysadmin', sysdate, sysdate, 'sysadmin', 1, NULL , NULL , 'Suppressed Alerts', 2, '/#!/alert/suppress');
 insert into menus values(6, 'sysadmin', sysdate, sysdate, 'sysadmin', 1, NULL , NULL , 'Closed Alerts', 2, '/#!/alert/closed');
 insert into menus values(7, 'sysadmin', sysdate, sysdate, 'sysadmin', 1, NULL , NULL , 'Create Alert', 2, '/#!/alert/create');
-insert into menus values(8, 'sysadmin', sysdate, sysdate, 'sysadmin', 1, NULL , NULL , 'Query', NULL, '/#!/dashboard');
-insert into menus values(9, 'sysadmin', sysdate, sysdate, 'sysadmin', 1, NULL , NULL , 'Reports', NULL, '/#!/dashboard');
-insert into menus values(10, 'sysadmin', sysdate, sysdate, 'sysadmin', 1, NULL , NULL , 'Setting', NULL, NULL);
-insert into menus values(11, 'sysadmin', sysdate, sysdate, 'sysadmin', 1, NULL , NULL , 'Scenario', 10, '/#!/dashboard');
-insert into menus values(12, 'sysadmin', sysdate, sysdate, 'sysadmin', 1, NULL , NULL , 'Workflow', 10, '/#!/workflow/index');
-insert into menus values(13, 'sysadmin', sysdate, sysdate, 'sysadmin', 1, NULL , NULL , 'Batch', 10, '/#!/batch');
-insert into menus values(14, 'sysadmin', sysdate, sysdate, 'sysadmin', 1, NULL , NULL , 'Document', 10, '/#!/document/index');
+insert into menus values(8, 'sysadmin', sysdate, sysdate, 'sysadmin', 1, NULL , NULL , 'Case', NULL, NULL);
+insert into menus values(9, 'sysadmin', sysdate, sysdate, 'sysadmin', 1, NULL , NULL , 'Query', NULL, NULL);
+insert into menus values(10, 'sysadmin', sysdate, sysdate, 'sysadmin', 1, NULL , NULL , 'Reports', NULL, NULL);
+insert into menus values(11, 'sysadmin', sysdate, sysdate, 'sysadmin', 1, NULL , NULL , 'Setting', NULL, NULL);
+insert into menus values(12, 'sysadmin', sysdate, sysdate, 'sysadmin', 1, NULL , NULL , 'Scenario', 11, '/#!/dashboard');
+insert into menus values(13, 'sysadmin', sysdate, sysdate, 'sysadmin', 1, NULL , NULL , 'Workflow', 11, '/#!/workflow/index');
+insert into menus values(14, 'sysadmin', sysdate, sysdate, 'sysadmin', 1, NULL , NULL , 'Batch', 11, '/#!/batch');
+insert into menus values(15, 'sysadmin', sysdate, sysdate, 'sysadmin', 1, NULL , NULL , 'Document', 11, '/#!/document/index');
+insert into menus values(16, 'sysadmin', sysdate, sysdate, 'sysadmin', 1, NULL , NULL , 'Report Summary', 10, '/#!/report/summary');
+insert into menus values(17, 'sysadmin', sysdate, sysdate, 'sysadmin', 1, NULL , NULL , 'Report Detail', 10, '/#!/report/detail');
+insert into menus values(18, 'sysadmin', sysdate, sysdate, 'sysadmin', 1, NULL , NULL , 'Alert Query', 9, '/#!/query/alert');
+insert into menus values(19, 'sysadmin', sysdate, sysdate, 'sysadmin', 1, NULL , NULL , 'Case Query', 9, '/#!/query/case');
+insert into menus values(20, 'sysadmin', sysdate, sysdate, 'sysadmin', 1, NULL , NULL , 'My Case', 8, '/#!/case/myCase');
+insert into menus values(21, 'sysadmin', sysdate, sysdate, 'sysadmin', 1, NULL , NULL , 'Closed Case', 8, '/#!/case/closed');
+
 
 --initial role menu table
 
@@ -77,11 +85,21 @@ insert into rolemenu values (11, 'sysadmin', sysdate, sysdate, 'sysadmin', 1, 11
 insert into rolemenu values (12, 'sysadmin', sysdate, sysdate, 'sysadmin', 1, 12, 6);
 insert into rolemenu values (13, 'sysadmin', sysdate, sysdate, 'sysadmin', 1, 13, 6);
 insert into rolemenu values (14, 'sysadmin', sysdate, sysdate, 'sysadmin', 1, 14, 6);
-insert into rolemenu values (15, 'sysadmin', sysdate, sysdate, 'sysadmin', 1, 1, 1);
-insert into rolemenu values (16, 'sysadmin', sysdate, sysdate, 'sysadmin', 1, 1, 2);
-insert into rolemenu values (17, 'sysadmin', sysdate, sysdate, 'sysadmin', 1, 1, 3);
-insert into rolemenu values (18, 'sysadmin', sysdate, sysdate, 'sysadmin', 1, 1, 4);
-insert into rolemenu values (19, 'sysadmin', sysdate, sysdate, 'sysadmin', 1, 1, 5);
+insert into rolemenu values (15, 'sysadmin', sysdate, sysdate, 'sysadmin', 1, 15, 6);
+insert into rolemenu values (16, 'sysadmin', sysdate, sysdate, 'sysadmin', 1, 16, 6);
+insert into rolemenu values (17, 'sysadmin', sysdate, sysdate, 'sysadmin', 1, 17, 6);
+insert into rolemenu values (18, 'sysadmin', sysdate, sysdate, 'sysadmin', 1, 18, 6);
+insert into rolemenu values (19, 'sysadmin', sysdate, sysdate, 'sysadmin', 1, 19, 6);
+insert into rolemenu values (20, 'sysadmin', sysdate, sysdate, 'sysadmin', 1, 20, 6);
+insert into rolemenu values (21, 'sysadmin', sysdate, sysdate, 'sysadmin', 1, 21, 6);
+
+
+
+insert into rolemenu values (22, 'sysadmin', sysdate, sysdate, 'sysadmin', 1, 1, 1);
+insert into rolemenu values (23, 'sysadmin', sysdate, sysdate, 'sysadmin', 1, 1, 2);
+insert into rolemenu values (24, 'sysadmin', sysdate, sysdate, 'sysadmin', 1, 1, 3);
+insert into rolemenu values (25, 'sysadmin', sysdate, sysdate, 'sysadmin', 1, 1, 4);
+insert into rolemenu values (26, 'sysadmin', sysdate, sysdate, 'sysadmin', 1, 1, 5);
 
 
 --initial rule scenario
