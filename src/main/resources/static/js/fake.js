5756341b22e5fc4c481b84f8626ffa238aa800e6
Iris Wen <iris.h.wen@pwc.com>
2017-07-17 13:51:50 +0800

    alert module page update

---
+++ src/main/resources/static/js/fake.js
@@ -0,0 +1,57 @@
+Mock.mock("/alert/myalert",[{
+    "alertId": 1,
+    "accNumber|1-10": 1,
+    "custName": "@first @last",
+    "createDate": "@date('yyyy-MM-dd')",
+    "scenario|1": ["level-1","level-2","level-3","level-4","level-5"],
+    "description": "@sentence(3,10)",
+    "triggeringValues|1-10.2-7": 1.00,
+    "status|1": ["Active","Closed","Deleted","Canceled"],
+    "age|1-100": 100
+    },
+    {
+        "alertId": 2,
+        "accNumber|1-10": 1,
+        "custName": "@first @last",
+        "createDate": "@date('yyyy-MM-dd')",
+        "scenario|1": ["level-1","level-2","level-3","level-4","level-5"],
+        "description": "@sentence(3,10)",
+        "triggeringValues|1-10.2-7": 1.00,
+        "status|1": ["Active","Closed","Deleted","Canceled"],
+        "age|1-100": 100
+    },
+    {
+        "alertId": 3,
+        "accNumber|1-10": 1,
+        "custName": "@first @last",
+        "createDate": "@date('yyyy-MM-dd')",
+        "scenario|1": ["level-1","level-2","level-3","level-4","level-5"],
+        "description": "@sentence(3,10)",
+        "triggeringValues|1-10.2-7": 1.00,
+        "status|1": ["Active","Closed","Deleted","Canceled"],
+        "age|1-100": 100
+    },
+    {
+        "alertId": 4,
+        "accNumber|1-10": 1,
+        "custName": "@first @last",
+        "createDate": "@date('yyyy-MM-dd')",
+        "scenario|1": ["level-1","level-2","level-3","level-4","level-5"],
+        "description": "@sentence(3,10)",
+        "triggeringValues|1-10.2-7": 1.00,
+        "status|1": ["Active","Closed","Deleted","Canceled"],
+        "age|1-100": 100
+    },
+    {
+        "alertId": 5,
+        "accNumber|1-10": 1,
+        "custName": "@first @last",
+        "createDate": "@date('yyyy-MM-dd')",
+        "scenario|1": ["level-1","level-2","level-3","level-4","level-5"],
+        "description": "@sentence(3,10)",
+        "triggeringValues|1-10.2-7": 1.00,
+        "status|1": ["Active","Closed","Deleted","Canceled"],
+        "age|1-100": 100
+    }
+
+])
