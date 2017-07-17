7f81aa90d45c90f914d63676f6d9f5da98bef75f
aliu323 <liu.y.andrew@pwc.com>
2017-07-17 17:26:08 +0800

    hdfsApi

--- src/main/java/com/pwc/aml/transation/dao/imp/HbaseDaoImp.java
+++ src/main/java/com/pwc/aml/transation/dao/imp/HbaseDaoImp.java
@@ -304,6 +304,7 @@ public class HbaseDaoImp implements IHbaseDao {
         //scan.setStartRow(Bytes.toBytes("20161119_10002"));
         //scan.setStopRow(Bytes.toBytes("20161119_10003"));
         Filter filter = new PrefixFilter(Bytes.toBytes("9000000000"));
+//        scan.set
         scan.setFilter(filter);
         //hbase conf
         //是否启动缓存
@@ -337,12 +338,13 @@ public class HbaseDaoImp implements IHbaseDao {
         RunShellTool tool = new RunShellTool("172.27.69.76", "hadoop", "Welcome1", 22, "utf-8");
         String result = null;
 		try {
-			result = tool.execSSH("../../../data/Hadoop/cdh/hadoop-2.5.0-cdh5.3.6/bin/yarn jar ../../data/Hadoop/cdh/hbase-0.98.6-hadoop2/lib/hbase-server-0.98.6-hadoop2.jar importtsv -Dimporttsv.columns=HBASE_ROW_KEY,f1:trans_id,f1:as_of_date,f1:acct_id,f1:trans_seq,f1:trans_chanel,f1:trans_cdt_cd,f1:currency_cd,f1:trans_base_amt,f1:trans_desc,f1:trans_dt,f1:counterparty_id_1,f1:trans_br,f1:trans_by aml:trans /user/hadoop/tmp/sampleData/tsvImport/TRANS");
+			result = tool.execSSH("../../../data/Hadoop/cdh/hadoop-2.5.0-cdh5.3.6/bin/yarn jar ../../data/Hadoop/cdh/hbase-0.98.6-hadoop2/lib/hbase-server-0.98.6-hadoop2.jar importtsv -Dimporttsv.columns=HBASE_ROW_KEY,f1:trans_id,f1:as_of_date,f1:acct_id,f1:trans_seq,f1:trans_chanel,f1:trans_cdt_cd,f1:currency_cd,f1:trans_base_amt,f1:trans_desc,f1:trans_dt,f1:counterparty_id_1,f1:trans_br,f1:trans_by aml:trans /user/hadoop/tmp/sampleData/tsvImport/trans_date");
 		} catch (IOException | JSchException e) {
 			e.printStackTrace();
 		}
         System.out.print(result);
     }
+
 
     public static void main(String[] args) throws Exception {
 //        System.out.println(System.getenv().get("HADOOP_HOME"));
