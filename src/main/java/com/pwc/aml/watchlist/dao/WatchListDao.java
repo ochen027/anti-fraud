1f05b11918f4b1e5e494d15af98454208e58fd32
wong.huang <wong.huang@pwc.com>
2017-07-24 09:58:57 +0800

    add watch list structure

---
+++ src/main/java/com/pwc/aml/watchlist/dao/WatchListDao.java
@@ -0,0 +1,7 @@
+package com.pwc.aml.watchlist.dao;
+
+/**
+ * Created by whuang072 on 7/21/2017.
+ */
+public class WatchListDao {
+}
---
+++ src/main/java/com/pwc/aml/watchlist/entity/WatchList.java
@@ -0,0 +1,7 @@
+package com.pwc.aml.watchlist.entity;
+
+/**
+ * Created by whuang072 on 7/21/2017.
+ */
+public class WatchList {
+}
