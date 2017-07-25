469df052c06e0d818f53ea28aa8dfd57df2e0905
Iris Wen <iris.h.wen@pwc.com>
2017-07-25 19:33:56 +0800

    csv file upload

--- src/main/java/com/pwc/aml/accounts/controller/AccountController.java
+++ src/main/java/com/pwc/aml/accounts/controller/AccountController.java
@@ -1,10 +1,38 @@
 package com.pwc.aml.accounts.controller;
 
-import com.pwc.common.base.controller.BaseController;
+import java.io.IOException;
+import java.text.ParseException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
+
+import com.pwc.aml.accounts.service.IAccountService;
+import com.pwc.common.base.controller.BaseController;
 
 @Controller
+@RequestMapping("account")
 public class AccountController extends BaseController{
+    @Autowired
+    private IAccountService accountService;
+
+
+    @RequestMapping(value = "/upload", method = RequestMethod.POST)
+    @ResponseBody
+    public ResponseEntity<?> upload(@RequestParam("file") MultipartFile file) throws IOException, ParseException {
+
+        if (!file.isEmpty()) {
+        	accountService.importCsvData(file);
+        }
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
 
 
 }
--- src/main/java/com/pwc/aml/accounts/service/AccountService.java
+++ src/main/java/com/pwc/aml/accounts/service/AccountService.java
@@ -1,7 +1,87 @@
 package com.pwc.aml.accounts.service;
 
+import au.com.bytecode.opencsv.CSVReader;
+import com.pwc.aml.accounts.dao.IAccountDAO;
+import com.pwc.aml.accounts.entity.Accounts;
+import org.apache.commons.io.FileUtils;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.List;
+import java.util.UUID;
 
 @Service
 public class AccountService implements IAccountService{
+
+    @Autowired
+    private IAccountDAO accountDAO;
+
+    private DateFormat df = new SimpleDateFormat("MM/dd/yyyy");
+
+    @Override
+    public void saveOrUpdateAccount(Accounts account) {
+
+        Accounts target = accountDAO.findByAcctId(account.getAccountId());
+
+        if (target == null) {
+            accountDAO.save(account);
+        } else {
+            accountDAO.update(account);
+        }
+    }
+
+    @Override
+    public Accounts findByAcctId(Accounts account) {
+        return accountDAO.findByAcctId(account.getAccountId());
+    }
+
+    @Override
+    public List<Accounts> findByCustId(Accounts account) {
+        return accountDAO.findByCustId(account.getCustomerId());
+    }
+
+    @Override
+    public List<Accounts> findAll() {
+        return null;
+    }
+
+    @Override
+    public void removeAll() {
+       // accountDAO.removeAll();
+    }
+
+
+    @Override
+    public void importCsvData(MultipartFile file) throws IOException, ParseException {
+
+        File temp = new File("temp", UUID.randomUUID().toString());
+        FileUtils.writeByteArrayToFile(temp, file.getBytes());
+        CSVReader reader = new CSVReader(new FileReader(temp));
+        List<String[]> accountList = reader.readAll();
+        for(int i=1;i<accountList.size();i++){
+            Accounts accounts=new Accounts();
+            String[] item=accountList.get(i);
+            accounts.setAccountId(item[1]);
+            accounts.setCustomerId(item[2]);
+            accounts.setAccountOpenDate(item[3].equals("")?null:df.parse(item[3]));
+            accounts.setAccountOpenBr(item[4]);
+            accounts.setAccountStatus(item[5]);
+            accounts.setAccountCloseDate(item[6].equals("")?null:df.parse(item[6]));
+            accounts.setAccountAmount(item[7].equals("") ? 0.00:Double.parseDouble(item[7]));
+            accounts.setLastUpdateBr(item[8]);
+            accounts.setLastUpdateDate(item[9].equals("") ? null:df.parse(item[9]));
+            accountDAO.save(accounts);
+        }
+        FileUtils.deleteQuietly(temp);
+    }
+
+
+
 }
