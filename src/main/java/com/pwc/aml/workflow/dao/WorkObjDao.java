956e22a6ef736572d645027458880a4f9c9cac78
wong.huang <wong.huang@pwc.com>
2017-07-28 15:18:57 +0800

    add workObjSchema

--- src/main/java/com/pwc/aml/workflow/dao/WorkObjDao.java
+++ src/main/java/com/pwc/aml/workflow/dao/WorkObjDao.java
@@ -2,6 +2,9 @@ package com.pwc.aml.workflow.dao;
 
 import com.pwc.aml.common.hbase.IHbaseDao;
 import com.pwc.aml.workflow.entity.WorkObj;
+import com.pwc.aml.workflow.entity.WorkObjSchema;
+import org.apache.hadoop.hbase.client.HTable;
+import org.codehaus.jackson.map.ObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
@@ -11,8 +14,30 @@ public class WorkObjDao implements IWorkObjDao {
     @Autowired
     private IHbaseDao hbaseDao;
 
-    @Override
-    public void save(WorkObj workObj) {
+    private HTable table;
 
+    private String rowKey;
+
+    private String tableKey="aml:trans";
+
+    @Override
+    public void save(WorkObj workObj) throws Exception {
+        ObjectMapper mapper=new ObjectMapper();
+        initial(workObj.getWorkObjId());
+        saveColumn(WorkObjSchema.workObjectId,workObj.getWorkObjId());
+        saveColumn(WorkObjSchema.flowId,workObj.getWorkflow().getFlowId());
+        saveColumn(WorkObjSchema.currentPointId,workObj.getCurrentPoint().getFlowPointId());
+        saveColumn(WorkObjSchema.historyEvents, mapper.writeValueAsString(workObj.getHistoryEvents()));
+        saveColumn(WorkObjSchema.roleId,""+workObj.getCurrentPoint().getRoleId());
     }
+
+    public void initial(String rowKey) throws Exception {
+        table = hbaseDao.getTable(tableKey);
+        this.rowKey=rowKey;
+    }
+
+    public void saveColumn(String key, String value) throws Exception {
+        hbaseDao.putData(table,rowKey,"f1", key,value);
+    }
+
 }
