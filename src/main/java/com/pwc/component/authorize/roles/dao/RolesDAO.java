93b0e1805766479a7d46e304095a59bce13b8e22
Iris Wen <iris.h.wen@pwc.com>
2017-07-28 15:17:20 +0800

    role module update

--- src/main/java/com/pwc/component/authorize/roles/dao/RolesDAO.java
+++ src/main/java/com/pwc/component/authorize/roles/dao/RolesDAO.java
@@ -1,14 +1,17 @@
 package com.pwc.component.authorize.roles.dao;
 
-import java.util.List;
-
-import javax.persistence.EntityManager;
-import javax.persistence.PersistenceContext;
-
+import com.pwc.component.authorize.roles.entity.Roles;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
-import com.pwc.component.authorize.roles.entity.Roles;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.TypedQuery;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Predicate;
+import javax.persistence.criteria.Root;
+import java.util.List;
 
 @Transactional
 @Repository
@@ -20,7 +23,42 @@ public class RolesDAO implements IRolesDAO{
 	@SuppressWarnings("unchecked")
 	@Override
 	public List<Roles> listAll() {
-		String hql = "FROM Roles WHERE status = 1 ORDER BY id";
-		return (List<Roles>)entityManager.createQuery(hql).getResultList();
+		CriteriaBuilder cb = entityManager.getCriteriaBuilder();
+		CriteriaQuery<Roles> cq = cb.createQuery(Roles.class);
+		Root<Roles> rootEntry = cq.from(Roles.class);
+
+		Predicate condition = cb.equal(rootEntry.get("status"),true);
+		cq.where(condition);
+
+		CriteriaQuery<Roles> all = cq.select(rootEntry);
+		TypedQuery<Roles> query = entityManager.createQuery(all);
+		List<Roles> roles = query.getResultList();
+		return roles;
 	}
+
+	@Override
+	public Roles createRoles(Roles ur) {
+		entityManager.persist(ur);
+		return ur;
+	}
+
+	@Override
+	public void updateRoles(Roles ur) {
+		Roles r = this.getRoles(ur.getId());
+		entityManager.merge(ur);
+		entityManager.flush();
+	}
+
+	@Override
+	public void deleteRoles(Roles ur) {
+		ur.setStatus(false);
+		this.updateRoles(ur);
+
+	}
+
+	@Override
+	public Roles getRoles(int roleId) {
+		return entityManager.find(Roles.class, roleId);
+	}
+
 }
