a6430fbcb09e9613558d0a2c444720a1cf37b36e
wong.huang <wong.huang@pwc.com>
2017-08-08 15:58:56 +0800

    add customer

--- src/main/java/com/pwc/aml/customers/entity/Corporate.java
+++ src/main/java/com/pwc/aml/customers/entity/Corporate.java
@@ -1,7 +1,12 @@
 package com.pwc.aml.customers.entity;
 
+import javax.persistence.Transient;
+
 /**
  * Created by whuang072 on 8/8/2017.
  */
 public class Corporate {
+
+    @Transient
+    private Representative representative;
 }
--- src/main/java/com/pwc/aml/customers/entity/CustomerBase.java
+++ src/main/java/com/pwc/aml/customers/entity/CustomerBase.java
@@ -1,7 +1,34 @@
 package com.pwc.aml.customers.entity;
 
+import javax.persistence.Transient;
+import java.math.BigDecimal;
+
 /**
- * Created by whuang072 on 8/8/2017.
+ * table name: @Table(name = "CUSTOMERS")
  */
 public class CustomerBase {
+
+
+
+
+    @Transient
+    private Individual individual;
+
+    @Transient
+    private Corporate corporate;
+
+    @Transient
+    private BigDecimal totalTransAmt;
+    @Transient
+    private int totalTransCount;
+    @Transient
+    private String transIdArray;
+    @Transient
+    private String accountIdArray;
+    @Transient
+    private String businessDate;
+    @Transient
+    private String alertCreationDate;
+    @Transient
+    private String alertId;
 }
