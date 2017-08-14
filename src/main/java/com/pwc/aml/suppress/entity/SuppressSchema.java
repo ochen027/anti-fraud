bdac647d03a06c132ab27b84b6b4501e101fab86
wong.huang <wong.huang@pwc.com>
2017-08-14 13:56:24 +0800

    add customer Service function

---
+++ src/main/java/com/pwc/aml/suppress/entity/SuppressSchema.java
@@ -0,0 +1,19 @@
+package com.pwc.aml.suppress.entity;
+
+
+public class SuppressSchema {
+
+    public static final String customersId="customersId";
+
+    public static final String ruleId="ruleId";
+
+    public static final String isPermanent="isPermanent";
+
+    public static final String endDate="endDate";
+
+    public static final String createdBy="createdBy";
+    public static final String createdDate="createdDate";
+    public static final String updateBy="updateBy";
+    public static final String updateDate="updateDate";
+    public static final String isActive="isActive";
+}
