0eec5c0da839090ec36977a1446ec7112cd12c68
egao035 <eitan.y.gao@pwc.com>
2017-08-30 12:21:10 +0800

    transfer the searchSuppress function from AlertsController into SuppressController; add export function into suppress page; fix a bug of searching suppress by creation date.

---
+++ src/main/java/com/pwc/aml/suppress/entity/RemoveSuppress.java
@@ -0,0 +1,15 @@
+package com.pwc.aml.suppress.entity;
+
+import java.util.List;
+
+public class RemoveSuppress {
+    private List<String> ids;
+
+    public List<String> getIds() {
+        return ids;
+    }
+
+    public void setIds(List<String> ids) {
+        this.ids = ids;
+    }
+}
